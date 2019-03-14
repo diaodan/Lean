f76b9a90ec03378be78bb0076b3bd8c75eb12fd2
李露 <lilu@loongstore.com.cn>
2019-03-14 03:50:29 -0400

    add autoconf lean

---
+++ autoconf/auto1/hello-1.0/clear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf  aclocal.m4 autom4te.cache autoscan.log \
+        config.log config.status configure configure.ac \
+        depcomp install-sh main.o Makefile Makefile.in \
+        missing sum.o val.o hello get.o
