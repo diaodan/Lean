24238eb706abaa67365803f79dd63dc81db1a736
李露 <lilu@loongstore.com.cn>
2019-03-18 03:49:54 -0400

    client server

---
+++ net/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#define STRCPY(a, b)    strcpy(a##_p, #b)
+#define STRCPY1(a, b)   strcpy(a##_p, b##_p)
+int main(void)  {
+                char var1_p[20];
+                                char var2_p[30];
+           strcpy(var1_p, "aaaa");
+                        strcpy(var2_p, "bbbb");
+                                        STRCPY1(var1, var2);
+                                                STRCPY(var2, var1);
+                                                        printf("var1 = %s\n",var1_p);
+                                printf("var2 = %s\n", var2_p);
+                                        return 0;
+}
+
