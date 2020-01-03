acc8db4969324cca3588ab2a14b5a078af5895b6
lilu <lilu@liludeMacBook-Pro.local>
2020-01-03 21:03:34 +0800

    base lite fs implement

--- fs/litefs/litefs.c
+++ fs/litefs/litefs.c
@@ -2,25 +2,123 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+struct inode_operations lite_fs_file_inode_iops = {
+
+};
+
+struct file_operations lite_fs_file_fops = {
+    .read   = do_sync_read,
+    .write  = do_sync_write,
+    .open   = generic_file_open,
+    .llseek = generic_file_llseek,
+};
+
+
+int lite_fs_create(struct inode *dir_inode, struct dentry *dentry,
+                            int mode, struct nameidata *nd)
+{
+    LOG_INFO();
+    return 0;
+}
+
+struct dentry *lite_fs_lookup(struct inode *dir_inode, struct dentry *dentry,
+                            struct nameidata *nd)
+{
+    LOG_INFO();
+    return NULL;
+}
+
+int lite_fs_mkdir(struct inode *dir_inode, struct dentry *dentry, int mode)
+{
+    LOG_INFO();
+    return 0;
+}
+
+int lite_fs_rm_dir(struct inode *dir_inode, struct dentry *dentry)
+{
+    LOG_INFO();
+    return 0;
+}
+
+int lite_fs_readdir(struct file *filp, void *dirent, filldir_t filldir)
+{
+    LOG_INFO();
+    return 0;
+}
+
+struct inode_operations lite_fs_dir_inode_iops = {
+    .create     = lite_fs_create,
+    .lookup     = lite_fs_lookup,
+    .mkdir      = lite_fs_mkdir,
+    .rmdir      = lite_fs_rm_dir,
+};    
+
+struct file_operations lite_fs_dir_fops = {
+    .llseek     = generic_file_llseek,
+    .read       = generic_read_dir,
+    .readdir    = lite_fs_readdir,
+};
+
+static struct super_operations lite_fs_sops = {
+//    .alloc_inode    = lite_fs_alloc_inode,
+//    .destroy_inode  = lite_fs_destroy_inode,
+//    .write_inode    = lite_fs_write_inode,
+};
+
+struct inode * lite_fs_iget(struct super_block *sb, unsigned long ino)
+{
+    struct inode *inode = NULL;
+
+    inode = iget_locked(sb, ino);
+    if (!inode) {
+        return ERR_PTR(-ENOMEM);
+    }
+    if (!(inode->i_state & I_NEW)) {
+        return inode;
+    }
+
+    inode->i_mode = S_IFDIR;
+
+    if (S_ISREG(inode->i_mode)) {
+        inode->i_op = &lite_fs_file_inode_iops;
+        inode->i_fop = &lite_fs_file_fops;
+    } else if (S_ISDIR(inode->i_mode)) {
+        inode->i_op = &lite_fs_dir_inode_iops;
+        inode->i_fop = &lite_fs_dir_fops;
+    } else {
+        LOG_ERR("LITE fs: unknown file type");
+        unlock_new_inode(inode);
+        iput(inode);
+        inode = NULL;
+        goto bad_inode;
+    }
+
+    unlock_new_inode(inode);
+    return inode;
+bad_inode:
+    return ERR_PTR(-EINVAL);
+}
+
+
 static int lite_fs_fill_super(struct super_block *sb, void *data, int silent)
 {
     LOG_INFO();
 
     struct buffer_head *bh = NULL;
     struct lite_fs_super_info *lsb = NULL;
-
     struct lite_fs_super_info *sbi = NULL;
+    unsigned int blocksize;
+    struct inode *root = NULL;
+    int ret = 0;
 
-    sbi = kzalloc(sizeof(*sbi), GFP_KERENL);
+    sbi = kzalloc(sizeof(*sbi), GFP_KERNEL);
     if (!sbi) {
-        return = -ENOMEM;
+        return  -ENOMEM;
     }
 
     sb->s_fs_info = sbi;
 
     blocksize = sb_set_blocksize(sb, 1024);
-    
-
 
     if (!(bh = sb_bread(sb, 1))) {
         LOG_ERR("unable to read superblock");
@@ -29,7 +127,55 @@ static int lite_fs_fill_super(struct super_block *sb, void *data, int silent)
 
     lsb = (struct lite_fs_super_info *)((char *)bh->b_data);
 
-    lsb->s_blocks_count = sb->s_bdev->bd_part->nr_sects;
+    //blocksize = 1k
+    lsb->s_blocks_count = sb->s_bdev->bd_part->nr_sects >>(blocksize >> 9);
+    lsb->s_inodes_count = 100;
+    lsb->s_free_blocks_count = lsb->s_blocks_count-1;
+    lsb->s_free_inodes_count = lsb->s_inodes_count-1;
+    LOG_INFO("%lu  %lu", lsb->s_free_blocks_count, lsb->s_free_inodes_count);
+    lsb->s_first_inode_bitmap = INODE_BITMAP_BLOCK;
+    lsb->s_first_data_bitmap = DATA_BITMAP_BLOCK;
+    lsb->s_first_inode_block = FIRST_INODE_BLOCK;
+    lsb->s_first_data_block = FIRST_DATA_BLOCK;
+    lsb->s_magic = LITE_MAGIC;
+
+
+    get_bh(bh);
+    lock_buffer(bh);
+    bh->b_end_io = end_buffer_write_sync;
+    submit_bh(WRITE, bh);
+    wait_on_buffer(bh);
+
+    sb->s_blocksize = LITE_BLOCKSIZE;
+    sb->s_maxbytes = LITE_MAX_FILE_SIZE;
+    sb->s_magic = LITE_MAGIC;
+    sb->s_blocksize_bits = LITE_BLOCKBITS;
+
+    sb->s_op = &lite_fs_sops;
+
+    root = lite_fs_iget(sb, 1);
+    if (IS_ERR(root)) {
+        ret = PTR_ERR(root);
+        goto failed_lsb;
+    }
+
+    sb->s_root = d_alloc_root(root);
+    if (!sb->s_root) {
+        iput(root);
+        LOG_ERR("LITE FS: get root inode failed\n");
+        goto failed_lsb;
+    }
+    brelse(bh);
+    return 0;
+
+failed_lsb:
+    LOG_ERR("LITE fs: failed fill super");
+    if (bh) {
+        brelse(bh);
+        bh = NULL;
+    }
+    return ret;
+}
 
 
 
@@ -44,6 +190,7 @@ static int lite_fs_get_sb(struct file_system_type *fs_type, int flags,
 static void lite_fs_kill_block_super(struct super_block *sb)
 {
     LOG_INFO();
+    kill_block_super(sb);
 }
 
 static struct file_system_type lite_fs_type = {
--- fs/litefs/litefs.h
+++ fs/litefs/litefs.h
@@ -13,6 +13,17 @@
 
 #define LOG_ERR(fmt, args...)   printk(KERN_ERR "ERROR: file %s, func %s, line %d: "fmt"\n", __FILE__,__func__, __LINE__, ##args)
 
+#define INODE_BITMAP_BLOCK  10
+#define DATA_BITMAP_BLOCK   20
+#define FIRST_INODE_BLOCK   40
+#define FIRST_DATA_BLOCK    50
+
+#define LITE_BLOCKBITS      10
+#define LITE_BLOCKSIZE      (1 << LITE_BLOCKBITS)
+#define LITE_MAX_FILE_SIZE  (1 << 30)
+#define LITE_MAGIC  12345
+
+
 struct lite_fs_super_info {
     __u64   s_blocks_count;
     __u64   s_inodes_count;
@@ -22,6 +33,7 @@ struct lite_fs_super_info {
     __u64   s_first_data_bitmap;
     __u64   s_first_inode_block;
     __u64   s_first_data_block;
+    __u64   s_magic
 };
 
 #endif //__LITEFS_H__
